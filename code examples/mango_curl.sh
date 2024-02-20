ef07a0a82ad1ff278406291b455805cba76454e2
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:41:05 +0200

    Adding code examples/mango_curl.sh

---
+++ code examples/mango_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://shop.mango.com/lt/men?gad_source=1&gclid=cjwkcaiaungubhakeiwagid4akiddksf_zk7lumx_nottzazyf1w3isfgemq2iutuocip9yuq_qmzhochoyqavd_bwe&gclsrc=aw.ds"}'
